84a73d54b0671c69b5b474b003710f2e831fe4eb
gs201 <gs201@DESKTOP-9N3I061>
2024-12-07 22:08:10 +0100

    changed some colours :)

--- front-end/components/header.tsx
+++ front-end/components/header.tsx
@@ -19,7 +19,7 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="p-3 mb-3 border-bottom bg-gradient-to-br from-gray-900 to-gray-600 flex flex-col items-center">
+    <header className="p-3 mb-3 border-bottom bg-gradient-to-br from-green-900 to-green-500 flex flex-col items-center">
       <a className="flex mb-2 md:mb-5 text-white-50 text-3xl text-gray-300">
         {t('app.title')}
       </a>
